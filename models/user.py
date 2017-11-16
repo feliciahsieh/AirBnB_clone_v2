5e48c5ed0d7e94b4ff925e34fa67c07602c9df77
wendysegura <118@holbertonschool.com>
2017-11-16 00:40:58 +0000

    works #7

--- models/user.py
+++ models/user.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python
 """ holds class User"""
-from models.base_model import BaseModel
+import models
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """Representation of a user """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    if models.storage_type == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
