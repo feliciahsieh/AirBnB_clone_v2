b6a2929bb736d251b585a861a60632515320778b
feliciahsieh <felicia@tixwriteoff.com>
2017-11-21 02:46:14 +0000

    going home

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
+import os, errno
 import tarfile
 import datetime
 import time
+from fabric.api import *
 
 
 def do_pack():
     """ do_pack - create a compressed tar file """
 
     now = datetime.datetime.now()
-    fn = "web_static_" + str(now.year) + str(now.month) + str(now.day) + str(now.hour) + str(now.minute) + str(now.second) + ".tgz"
+    fn = "web_static_" + str(now.year) + str(now.month) + str(now.day) + str(now.hour) + str(now.minute) + str(now.
+second) + ".tgz"
     print(fn)
 
-
-    #print('{%Y%m%d%H%M%S}'.format(datetime.datetime.now()))
-
-    # web_static_<year><month><day><hour><minute><second>.tgz
-
     tar = tarfile.open(fn, "w")
-    for name in ["."]:
+    for name in ["/data/web_static"]:
         tar.add(name)
-    #tar.add("/data/web_static/releases/test/index.html")
-    #tar.add("/data/web_static/shared")
 
     tar.close()
+
+    directory = "./versions"
+    result = run("mkdir " + directory)
+    if result.failed:
+        print("failed mkdir()")
+    else:
+        print(directory)
 
 
 do_pack()
