efc895afbaf3342583d02aa45c57792da4d3a764
feliciahsieh <felicia@tixwriteoff.com>
2017-11-21 06:58:12 +0000

    more fabric-oriented

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python3
 import datetime
-from fabric.api import local
-from fabric.api import *
+from fabric.api import local, run, env
+
 
 def do_pack():
     """ do_pack - create a compressed tar file """
+    """ Ref: https://gist.github.com/elliottb/7744008 """
 
+    # remote ssh credentials
+    env.hosts = ['142.44.167.237']
+    env.user = 'ubuntu'
+    env.key_filename = '~/.ssh/holberton_key'
+
+    # specify path to files being deployed
+    env.archive_source = '/data/web_static'
+
+    # archive name, arbitrary, and only for transport
     now = datetime.datetime.now()
-    fn = "web_static_" + str(now.year) + str(now.month) + \
+    env.archive_name = "web_static_" + str(now.year) + str(now.month) + \
  str(now.day) + str(now.hour) + str(now.minute) + str(now.second) + ".tgz"
 
-    dirSrc = "/data/web_static"
-    dirDest = "versions"
-    result = run("mkdir " + dirDest)
+    result = run("mkdir versions")
     if result.failed:
         return None
 
-    result = cxn.run('tar -cvzf ' + dirDest + '/' + fn + ' web_static')
+    # specify path to deploy root dir - you need to create this
+    env.deploy_project_root = '/data/web_static/'
+
+    # specify name of dir that will hold all deployed code
+    env.deploy_release_dir = 'versions/'
+
+    # symlink name. Full path to deployed code is env.deploy_project_root + this
+    env.deploy_current_dir = 'current'
+
     if result.failed:
         return None
     else:
-        return(fn)
+        return(env.archive_name)
