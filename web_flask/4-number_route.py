a1cfc59cce5fa45eb57d2037abad8392c06afc5b
feliciahsieh <felicia@tixwriteoff.com>
2017-12-08 19:15:21 +0000

    working version

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""4-number_route.py"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """hello_hbnb"""
+    return 'Hello HBNB!'
+
+@app.route('/nbnb/', strict_slashes=False)
+def hbnb():
+    """hbnb"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def cWithVariable(text):
+    """cWithVariable"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>/', strict_slashes=False)
+def pythonWithVariable(text="is cool"):
+    """pythonWithVariable"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+@app.route('/number/<int:n>/', strict_slashes=False)
+def withNumber(n):
+    """withNumber"""
+    return "{} is a number".format(n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
