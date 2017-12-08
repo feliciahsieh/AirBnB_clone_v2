6fa7854bb47c6090c9b2534aa667dcb9026ddc6c
feliciahsieh <felicia@tixwriteoff.com>
2017-12-08 20:12:44 +0000

    working version

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""5-number_template.py"""
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """hello_hbnb"""
+    return 'Hello HBNB!'
+
+@app.route('/nbnb/', strict_slashes=False)
+def hbnb():
+    """hbnb"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def cWithVariable(text):
+    """cWithVariable"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>/', strict_slashes=False)
+def pythonWithVariable(text="is cool"):
+    """pythonWithVariable"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+@app.route('/number/<int:n>/', strict_slashes=False)
+def withNumber(n):
+    """withNumber"""
+    return "{} is a number".format(n)
+
+@app.route('/number_template/<int:n>/', strict_slashes=False)
+def withNumberSendWebpage(n=None):
+    """withNumberSendWebpage"""
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
